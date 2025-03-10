22c55cf359af187aded45a54b61fa3246f647085
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2025-03-10 16:07:13 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://chat-app-back-end-w40e.onrender.com",
   withCredentials: true,
 });
